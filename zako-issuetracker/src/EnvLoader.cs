25796f8986ff7e8da59d10ea5eedf01f5269c94e
200mill <200mill@naver.com>
2025-12-14 23:40:14 +0900

    Created EnvLoader class

---
+++ zako-issuetracker/src/EnvLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zako_issuetracker;
+
+public static class EnvLoader
+{
+    private static Dictionary<string, string?> GetEnv()
+    {
+        Dictionary<string, string?> EnvDict = new Dictionary<string, string?>();
+        EnvDict.Add("DISCORD_TOKEN", Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? null);
+        EnvDict.Add("SQLITE_FILE", Environment.GetEnvironmentVariable("SQLITE_FILE") ?? null);
+
+        return EnvDict;
+    }
+
+    public static string? GetToken()
+    {
+        return GetEnv()["DISCORD_TOKEN"] ?? null;
+    }
+
+    public static string? GetSqlitePath()
+    {
+        return GetEnv()["SQLITE_FILE"] ?? null;
+    }
+}
